9446e2025b763ed51db31bc162602b0e033e8c2d
Ryan Zemlin <rzemlin@gmail.com>
2021-02-07 17:29:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,15 @@
-#write your code here
+def countdown(count)
+    while count >0
+      puts "#{count} seconds!"
+      count-=1
+    end
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
-  counter == 10
-  
-end
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "HAPPY NEW YEAR!"
+  end
